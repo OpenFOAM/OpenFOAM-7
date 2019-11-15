ca808c8420bf6f92640a723e576ab79c830d5ed2
Henry Weller <http://openfoam.org>
2019-11-15 09:01:19 +0000

    combustionModels::laminar: Reinstated non-integrating reaction rate evaluation
    Resolves bug-report https://bugs.openfoam.org/view.php?id=3389

--- src/combustionModels/laminar/laminar.C
+++ src/combustionModels/laminar/laminar.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2013-2018 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2013-2019 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -109,6 +109,10 @@ void Foam::combustionModels::laminar<ReactionThermo>::correct()
         {
             this->chemistryPtr_->solve(this->mesh().time().deltaTValue());
         }
+    }
+    else
+    {
+        this->chemistryPtr_->calculate();
     }
 }
 
